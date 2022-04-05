91fdc4fa0345777752aef1b98d01cfdc537c28de
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-04-05 17:46:32 +0100

    Modified the scripts.js file to fix linters erroe

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
-
 const toggleMenu = document.querySelector('.toggle');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   toggleMenu.classList.toggle('active');
-})
+});
 
 document.querySelectorAll('.toggle-item').forEach(n => n.
   addEventListener('click', () => {
