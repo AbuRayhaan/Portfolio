b93e209d5821fbabd843a351a02f24b5bed78a47
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-04-05 20:47:25 +0100

    Modified style-script files

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
 const toggleMenu = document.querySelector('.toggle');
+const headline = document.querySelector ('.headline');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   toggleMenu.classList.toggle('active');
+  headline.classList.toggle('active');
 });
 
 document.querySelectorAll('.toggle-item').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   toggleMenu.classList.remove('active');
+  headline.classList.remove('active');
 }));
